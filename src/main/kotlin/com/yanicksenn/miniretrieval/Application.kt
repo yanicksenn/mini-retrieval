d6c2fe8b10db4e0d46f14fe4fe1b5464e7a7d33e
Yanick Senn <sennyan2@students.zhaw.ch>
2022-07-27 11:55:24 +0200

    Parse english and german stop-list

--- src/main/kotlin/com/yanicksenn/miniretrieval/Application.kt
+++ src/main/kotlin/com/yanicksenn/miniretrieval/Application.kt
@@ -1,7 +1,10 @@
 package com.yanicksenn.miniretrieval
 
 import com.yanicksenn.miniretrieval.indexer.IIndexer
+import com.yanicksenn.miniretrieval.stoplist.StopList
+import com.yanicksenn.miniretrieval.stoplist.StopListParser
 import java.io.File
+import java.io.InputStream
 
 /**
  * Wrapper for the actual business logic.
@@ -10,7 +13,21 @@ class Application(
     private val documentsRoot: File,
     private val indexer: IIndexer) : Runnable {
 
+    private val stopLists = HashMap<String, StopList>()
+
     override fun run() {
+        parseStopList("english")
+        parseStopList("german")
+
         indexer.addFilesToIndexRecursively(documentsRoot)
+    }
+
+    private fun parseStopList(language: String) {
+        stopLists[language] = StopListParser(getResourceAsStream("/stopwords/$language.txt")).parse()
+        println("Parsed $language stop-list with ${stopLists[language]!!.size} tokens")
+    }
+
+    private fun getResourceAsStream(resourceName: String): InputStream {
+        return javaClass.getResourceAsStream(resourceName) ?: throw RuntimeException("resource $resourceName does not exist")
     }
 }
