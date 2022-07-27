c0c8dfb7572e304cfff9a45b7a820efa4239a358
Yanick Senn <sennyan2@students.zhaw.ch>
2022-07-27 13:09:28 +0200

    Add documentation to stop-list parser

--- src/main/kotlin/com/yanicksenn/miniretrieval/stoplist/StopListParser.kt
+++ src/main/kotlin/com/yanicksenn/miniretrieval/stoplist/StopListParser.kt
@@ -2,12 +2,24 @@ package com.yanicksenn.miniretrieval.stoplist
 
 import java.io.InputStream
 
+/**
+ * Parser for stop lists.
+ */
 class StopListParser(private val stopListInputStream: InputStream) {
+
+    companion object {
+        /**
+         * Regex to match comment lines. Comment lines
+         * may start with whitespaces, followed by #,
+         * and then followed by anything.
+         */
+        private val COMMENTS_REGEX = "^\\s*#.*".toRegex()
+    }
 
     fun parse(): Set<String> {
         return stopListInputStream.bufferedReader()
             .lineSequence()
-            .filterNot { it.startsWith("#") }
+            .filterNot { it.matches(COMMENTS_REGEX) }
             .filterNot { it.isBlank() }
             .map { it.trim() }
             .toHashSet()
