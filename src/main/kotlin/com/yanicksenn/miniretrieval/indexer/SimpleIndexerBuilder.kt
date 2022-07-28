0cbbe2956dcdd9c02d280ca1ec8921fe504caafe
Yanick Senn <ys-services@yanicksenn.com>
2022-07-28 14:14:40 +0200

    Use simple tokenizer builder

--- src/main/kotlin/com/yanicksenn/miniretrieval/indexer/SimpleIndexerBuilder.kt
+++ src/main/kotlin/com/yanicksenn/miniretrieval/indexer/SimpleIndexerBuilder.kt
@@ -2,13 +2,12 @@ package com.yanicksenn.miniretrieval.indexer
 
 import com.yanicksenn.miniretrieval.language.LexiconsBuilder
 import com.yanicksenn.miniretrieval.stoplist.StopListsBuilder
-import com.yanicksenn.miniretrieval.tokenizer.SimpleNormalizer
-import com.yanicksenn.miniretrieval.tokenizer.SimpleTokenizer
+import com.yanicksenn.miniretrieval.tokenizer.SimpleTokenizerBuilder
 
 object SimpleIndexerBuilder {
     fun build(): SimpleIndexer {
         return SimpleIndexer(
-            SimpleTokenizer(SimpleNormalizer()),
+            SimpleTokenizerBuilder.build(),
             StopListsBuilder.build(),
             LexiconsBuilder.build()
         )
