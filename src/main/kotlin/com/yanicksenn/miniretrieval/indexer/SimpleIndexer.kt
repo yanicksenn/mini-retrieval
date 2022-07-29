ef59030865f8e2211cfbfbd8b038949c4e982583
Yanick Senn <ys-services@yanicksenn.com>
2022-07-29 17:05:54 +0200

    Stem stop-lists and shorten simple indexer class

--- src/main/kotlin/com/yanicksenn/miniretrieval/indexer/SimpleIndexer.kt
+++ src/main/kotlin/com/yanicksenn/miniretrieval/indexer/SimpleIndexer.kt
@@ -15,12 +15,24 @@ class SimpleIndexer(
     private val lexicons: Map<Language, Set<String>>,
     private val stemmers: Map<Language, IStemmer>) : IIndexer {
 
+    private val stemmedStopLists = HashMap<Language, HashSet<String>>()
+
     private val documents = HashMap<Document, Document>()
     private val tokens = HashMap<String, String>()
 
     private val tokensByDocumentIndex = HashMap<Document, HashMap<String, Int>>()
     private val documentsByTokenIndex = HashMap<String, HashMap<Document, Int>>()
 
+    init {
+        stopLists.keys.forEach { language ->
+            val stemmer = stemmers[language]!!
+            val stopList = stopLists[language]!!
+
+            stemmedStopLists[language] = stopList.map { token ->
+                stemmer.stem(token)
+            }.toHashSet()
+        }
+    }
 
     override fun findTokensByDocument(document: Document): Map<String, Int> {
         return tokensByDocumentIndex[document] ?: emptyMap()
@@ -44,25 +56,24 @@ class SimpleIndexer(
 
         println(file.absolutePath)
 
-        val text = file.readText().lowercase()
-        val languageResult = determineLanguage(text)
-
         // O(n) parsing file and tokenize
-        val document = getDocumentReference(Document(file.absolutePath))
-        when (languageResult) {
+        val text = file.readText().lowercase()
+        when (val languageResult = determineLanguage(text)) {
             is LanguageDeterminer.Nothing -> return
             is LanguageDeterminer.Match -> {
                 val languages = languageResult.languages
                 for (language in languages) {
-                    val tokens = tokenizeText(text, language)
-                    stemTokensAndAddToIndices(tokens, language, document)
+                    val tokenizer = tokenizers[language]!!
+                    val stemmer = stemmers[language]!!
+                    val stopList = stemmedStopLists[language]!!
+
+                    tokenizer.tokenize(text)
+                        .map { stemmer.stem(it) }
+                        .filterNot { stopList.contains(it) }
+                        .forEach { addToIndices(Document(file.absolutePath), it) }
                 }
             }
         }
-    }
-
-    private fun tokenizeText(text: String, language: Language): List<String> {
-        return tokenizers[language]!!.tokenize(text)
     }
 
     private fun determineLanguage(text: String): LanguageDeterminer.Result {
@@ -71,46 +82,10 @@ class SimpleIndexer(
         return languageDeterminer.currentLanguage
     }
 
-    private fun getDocumentReference(potentialDocumentReference: Document): Document {
-        return documents.getOrPut(potentialDocumentReference) { potentialDocumentReference }
-    }
-
-    private fun getTokenReference(potentialTokenReference: String): String {
-        return tokens.getOrPut(potentialTokenReference) { potentialTokenReference }
-    }
-
-    private fun stemTokensAndAddToIndices(tokens: List<String>, language: Language, document: Document) {
-        val stemmedTokens = stemTokens(tokens, language)
-        addToIndicesWithLanguage(document, stemmedTokens, language)
-    }
-
-    private fun stemTokens(tokens: List<String>, language: Language): List<String> {
-        val stemmer = stemmers[language]!!
-        return tokens.map { stemmer.stem(it) }
-    }
-
-    private fun addToIndicesWithLanguage(
-        document: Document,
-        tokens: List<String>,
-        language: Language
-    ) {
-        // O(n) filter tokens and adding them to the indices
-        val stopList = stopLists[language] ?: emptySet()
-        tokens
-            .filterNot { stopList.contains(it) }
-            .map { getTokenReference(it) }
-            .forEach { token ->
-                addToDocumentIndex(document, token)
-                addToInvertedDocumentIndex(document, token)
-            }
-    }
-
-    private fun addToDocumentIndex(document: Document, token: String) {
+    private fun addToIndices(document: Document, token: String) {
         val tokens = tokensByDocumentIndex.getOrPut(document) { HashMap() }
         tokens[token] = tokens.getOrDefault(token, 0) + 1
-    }
 
-    private fun addToInvertedDocumentIndex(document: Document, token: String) {
         val documents = documentsByTokenIndex.getOrPut(token) { HashMap() }
         documents[document] = documents.getOrDefault(document, 0) + 1
     }
