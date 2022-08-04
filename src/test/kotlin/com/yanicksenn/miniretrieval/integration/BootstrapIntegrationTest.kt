141630fafbc4cb3e11f78891d6d5c2c4e0dccd43
Yanick Senn <ys-services@yanicksenn.com>
2022-08-04 10:31:42 +0200

    Disable bootstrap integration test

--- src/test/kotlin/com/yanicksenn/miniretrieval/integration/BootstrapIntegrationTest.kt
+++ src/test/kotlin/com/yanicksenn/miniretrieval/integration/BootstrapIntegrationTest.kt
@@ -1,15 +1,20 @@
 package com.yanicksenn.miniretrieval.integration
 
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 import java.io.File
 
 
+/**
+ * This class contains simple bootstrap integration tests.
+ */
 class BootstrapIntegrationTest : AbstractIntegrationTest() {
 
     private val documentsDirectory = File("src/test/resources/documents")
 
     @Test
+    @Disabled
     fun `program iterates through files in documents directory`() {
         assertDoesNotThrow { runIntegrationTest(documentsDirectory.absolutePath) }
     }
