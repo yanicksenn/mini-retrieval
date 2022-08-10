15b014e05922854fadc6326ea293b4eb6643836b
Yanick Senn <ys-services@yanicksenn.com>
2022-08-10 11:49:57 +0200

    Remove temporary pipelines

--- src/main/kotlin/com/yanicksenn/miniretrieval/TFIDF.kt
+++ src/main/kotlin/com/yanicksenn/miniretrieval/TFIDF.kt
@@ -2,14 +2,26 @@ package com.yanicksenn.miniretrieval
 
 import com.yanicksenn.miniretrieval.indexer.StringFrequency
 import com.yanicksenn.miniretrieval.indexer.StringFrequencyByKey
-import com.yanicksenn.miniretrieval.pipeline.ParsingPipeline
-import com.yanicksenn.miniretrieval.pipeline.TokenizationPipeline
+import com.yanicksenn.miniretrieval.language.Language
+import com.yanicksenn.miniretrieval.language.LanguageDeterminer
+import com.yanicksenn.miniretrieval.language.LexiconsBuilder
+import com.yanicksenn.miniretrieval.stemmer.StemmersBuilder
+import com.yanicksenn.miniretrieval.stoplist.StopListsBuilder
+import com.yanicksenn.miniretrieval.to.Token
+import com.yanicksenn.miniretrieval.tokenizer.DefaultTokenizer
+import com.yanicksenn.miniretrieval.tokenizer.TokenizersBuilder
 import java.io.File
 
 /**
  * TFIDF ranking model.
  */
 class TFIDF(private val documentsRoot: File) {
+    private val lexicons = LexiconsBuilder.build()
+    private val stemmers = StemmersBuilder.build()
+    private val stopLists = StopListsBuilder.build()
+    private val tokenizers = TokenizersBuilder.build()
+    private val defaultTokenizer = DefaultTokenizer
+
     private val documentIndex = StringFrequencyByKey()
     private val tokenIndex = StringFrequencyByKey()
 
@@ -22,34 +34,73 @@ class TFIDF(private val documentsRoot: File) {
         documentsRoot.walk()
             .filter { it.isFile }
             .filterNot { documentIndex.contains(it.absolutePath) }
-            .forEach { index(it) }
+            .forEach { indexFile(it) }
 
         return this
     }
 
     /**
-     * Indexes the given file.
-     * @param file File
-     */
-    fun index(file: File) {
-        val (document, textRaw) = ParsingPipeline.pipeline(file)
-        TokenizationPipeline.pipeline(textRaw)
-            .forEach {
-                documentIndex.add(document, it)
-                tokenIndex.add(it, document)
-            }
-    }
-
-    /**
      * Queries the relevant documents based on the
      * given raw query.
-     * @param queryRaw Raw query
+     * @param rawQuery Raw query
      */
-    fun query(queryRaw: String): List<RSV.Result> {
+    fun query(rawQuery: String): List<RSV.Result> {
         val queryFrequency = StringFrequency()
-        TokenizationPipeline.pipeline(queryRaw)
-            .forEach { queryFrequency.add(it) }
+        val tokens = tokenizeRawText(rawQuery)
+        tokens.forEach { queryFrequency.add(it) }
 
         return RSV(documentIndex, tokenIndex, queryFrequency).query()
+    }
+
+    private fun indexFile(file: File) {
+        val (document, rawText) = parseFile(file)
+        val tokens = tokenizeRawText(rawText)
+        tokens.forEach { token ->
+            documentIndex.add(document, token)
+            tokenIndex.add(token, document)
+        }
+    }
+
+    private fun parseFile(file: File): Pair<String, String> {
+        val document = file.absolutePath
+        val rawText = file.readText()
+        return Pair(document, rawText)
+    }
+
+    private fun tokenizeRawText(textRaw: String): List<Token> {
+        val text = sanitizeRawText(textRaw)
+        val tokensRaw = tokenizeUnlocalizedText(text)
+
+        return when(val result = localizeRawTokens(tokensRaw)) {
+            is LanguageDeterminer.Nothing, LanguageDeterminer.Unsure -> tokensRaw
+            is LanguageDeterminer.Match -> tokenizeLocalizedText(text, result.language)
+        }
+    }
+
+    private fun sanitizeRawText(rawText: String): String {
+        return rawText.lowercase()
+    }
+
+    private fun localizeRawTokens(rawTokens: List<Token>): LanguageDeterminer.Result {
+        val languageDeterminer = LanguageDeterminer(lexicons)
+        rawTokens.forEach { token ->
+            languageDeterminer.readToken(token)
+        }
+
+        return languageDeterminer.currentLanguage
+    }
+
+    private fun tokenizeUnlocalizedText(text: String): List<Token> {
+        return defaultTokenizer.tokenize(text)
+    }
+
+    private fun tokenizeLocalizedText(text: String, language: Language): List<Token> {
+        val stemmer = stemmers[language]!!
+        val stopList = stopLists[language]!!
+        val tokenizer = tokenizers[language]!!
+
+        return tokenizer.tokenize(text)
+            .map { stemmer.stem(it) }
+            .filterNot { stopList.contains(it) }
     }
 }
--- src/main/kotlin/com/yanicksenn/miniretrieval/pipeline/SanitizationPipeline.kt
+++
@@ -1,7 +0,0 @@
-package com.yanicksenn.miniretrieval.pipeline
-
-object SanitizationPipeline {
-    fun pipeline(text: String): String {
-        return text.lowercase()
-    }
-}
