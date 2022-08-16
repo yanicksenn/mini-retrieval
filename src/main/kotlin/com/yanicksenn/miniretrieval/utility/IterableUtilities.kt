52a5ed5ae646219dbf6d2bcba7c7f3c66f2120eb
Yanick Senn <ys-services@yanicksenn.com>
2022-08-16 08:44:19 +0200

    Remove unused utilities

--- src/main/kotlin/com/yanicksenn/miniretrieval/utility/IterableUtilities.kt
+++
@@ -1,6 +0,0 @@
-package com.yanicksenn.miniretrieval.utility
-
-inline fun <T> Iterable<T>.forEachAnd(action: (T) -> Unit): Iterable<T> {
-    for (element in this) action(element)
-    return this
-}
