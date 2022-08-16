e147465aebf74277cf546e3c764f9d78ee65c898
Yanick Senn <ys-services@yanicksenn.com>
2022-08-16 17:54:58 +0200

    Enable bootstrap test again

--- src/test/kotlin/com/yanicksenn/miniretrieval/integration/BootstrapIntegrationTest.kt
+++ src/test/kotlin/com/yanicksenn/miniretrieval/integration/BootstrapIntegrationTest.kt
@@ -1,6 +1,5 @@
 package com.yanicksenn.miniretrieval.integration
 
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 import java.io.File
@@ -14,7 +13,6 @@ class BootstrapIntegrationTest : AbstractIntegrationTest() {
     private val documentsDirectory = File("src/test/resources/documents")
 
     @Test
-    @Disabled
     fun `should bootstrap application`() {
         assertDoesNotThrow { runIntegrationTest("${documentsDirectory.absolutePath} \"who is max verstappen\"") }
     }
