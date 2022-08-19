d5c95270f6af217cc1d89b90dc5d0c66df2cfcf6
Yanick Senn <ys-services@yanicksenn.com>
2022-08-19 16:57:09 +0200

    Add layer of abstraction for pdf parser

---
+++ src/main/kotlin/com/yanicksenn/miniretrieval/adapter/pdfbox/PDFBoxDocumentParser.kt
@@ -0,0 +1,26 @@
+package com.yanicksenn.miniretrieval.adapter.pdfbox
+
+import com.yanicksenn.miniretrieval.adapter.IDocumentParser
+import com.yanicksenn.miniretrieval.to.Document
+import org.apache.pdfbox.pdmodel.PDDocument
+import org.apache.pdfbox.text.PDFTextStripper
+import java.io.File
+
+object PDFBoxDocumentParser : IDocumentParser {
+    override fun parse(file: File): Sequence<Document> {
+        return sequence {
+            PDDocument.load(file).use { document ->
+                val documentIdPrefix = file.absolutePath
+                for (i in 1 .. document.numberOfPages) {
+                    val reader = PDFTextStripper()
+                    reader.startPage = i
+                    reader.endPage = i
+
+                    val documentId = "$documentIdPrefix#$i"
+                    val text = reader.getText(document)
+                    yield(Document(documentId, text))
+                }
+            }
+        }
+    }
+}
