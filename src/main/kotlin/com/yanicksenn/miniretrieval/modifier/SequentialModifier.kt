f168ac3a51a30478b21b73e40488266ff8ae9320
Yanick Senn <ys-services@yanicksenn.com>
2022-08-30 16:23:30 +0200

    Add documentation to sequential modifier

--- src/main/kotlin/com/yanicksenn/miniretrieval/modifier/SequentialModifier.kt
+++ src/main/kotlin/com/yanicksenn/miniretrieval/modifier/SequentialModifier.kt
@@ -2,6 +2,10 @@ package com.yanicksenn.miniretrieval.modifier
 
 import com.yanicksenn.miniretrieval.to.DocumentId
 
+/**
+ * Sequentially applies the provided modifiers in the
+ * given order and calculates the new score.
+ */
 class SequentialModifier(private vararg val modifiers: IModifier) : IModifier {
     override fun modify(documentId: DocumentId, score: Double): Double {
         var newScore = score
